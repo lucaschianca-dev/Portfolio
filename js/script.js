787c2a41fc3fc30d61ea0de80c91174b74b72c2d
Lucas Chianca <lucaseduardochianca.dev@gmail.com>
2024-05-22 13:58:18 -0300

    finalizado a feature do menu responsivo

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 const menuResponsive = document.querySelector('.menu-responsive');
 
 menuResponsive.addEventListener('click', () => {
-    console.log("OK");
+    toggleMenu();
 });
+
+function toggleMenu() {
+    const nav = document.querySelector('.nav-responsive');
+    menuResponsive.classList.toggle('change');
+
+    if (menuResponsive.classList.contains('change')) {
+        nav.style.display = 'block';
+    } else {
+        nav.style.display = 'none';
+    }
+}
